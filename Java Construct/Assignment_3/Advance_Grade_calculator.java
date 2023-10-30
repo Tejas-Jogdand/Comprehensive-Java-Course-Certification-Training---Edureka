2cd2a7d08a1468bd68f6ecc97e4584689fec350a
unknown <tejasjogdand37@gmail.com>
2023-10-30 18:42:14 +0530

    A3

---
+++ Java Construct/Assignment_3/Advance_Grade_calculator.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Advance_Grade_calculator {
+    
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the marks of subjects (out of /100):");
+        System.out.println("Maths:");
+        int Maths = sc.nextInt();
+        System.out.println("English:");
+        int English = sc.nextInt();
+        System.out.println("Hindi:");
+        int Hindi = sc.nextInt();
+        System.out.println("Science:");
+        int Science = sc.nextInt();
+        System.out.println("Social Science:");
+        int SocialScience = sc.nextInt();
+
+        if (Maths>100 || English>100 ||) {
+            
+        }
+        float total = Maths + English + Hindi + Science + SocialScience;
+
+        float percentage = (total/500)*100;
+
+        System.out.println("Percentage : " + percentage +"\nGrade : " );
+
+        if(percentage<40){
+            System.out.println("Poor");
+        }
+        else if(percentage<60 && percentage>=40){
+            System.out.println("Average");
+        }
+        else if(percentage<80 && percentage>=60){
+            System.out.println("Good");
+        }
+        else if(percentage<90 && percentage>=80){
+            System.out.println("Very Good");
+        }
+        else if(percentage>=90){
+            System.out.println("Excellent");
+        }
+    }
+}
